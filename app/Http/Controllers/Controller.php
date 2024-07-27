00735d41bb349b9b271fbecff2f0999d88112475
Gauthier EHOLOUM <thiergau691@gmail.com>
2024-07-27 01:25:59 +0000

    chore: add swagger documentation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,17 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *     version="1.0",
+ *     title="Laravel Modules API",
+ * )
+ * @OA\Get(
+ *     path="/",
+ *     description="Home page",
+ *     @OA\Response(response="default", description="Welcome page")
+ * )
+ */
 abstract class Controller
 {
-    //
 }
--- app/Http/Middleware/ModuleLoader.php
+++ app/Http/Middleware/ModuleLoader.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Nwidart\Modules\Facades\Module;
+
 use App\Models\Module as ModuleModel;
 
 class ModuleLoader
@@ -14,57 +15,31 @@ class ModuleLoader
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
     public function handle(Request $request, Closure $next): Response
     {
         $routeName = Route::currentRouteName();
         $moduleName = $this->getModuleNameFromRoute($routeName);
 
-        if ($moduleName) {
-            if ($this->isModuleActive($moduleName)) {
-                $this->loadModule($moduleName);
-            } else {
-                return response()->json([
-                    'message' => "The module '{$moduleName}' is disabled.",
-                ], 403); // Forbidden
-            }
+        if ($moduleName && $this->isModuleActive($moduleName)) {
+            $this->loadModule($moduleName);
         }
 
         return $next($request);
     }
 
-    /**
-     * Extract module name from route name.
-     *
-     * @param  string  $routeName
-     * @return string|null
-     */
     protected function getModuleNameFromRoute($routeName)
     {
         $segments = explode('.', $routeName);
         return $segments[0] ?? null;
     }
 
-    /**
-     * Check if the module is active.
-     *
-     * @param  string  $moduleName
-     * @return bool
-     */
     protected function isModuleActive($moduleName)
     {
         return ModuleModel::where('name', $moduleName)->where('is_active', true)->exists();
     }
 
-    /**
-     * Load the module if it's active.
-     *
-     * @param  string  $moduleName
-     * @return void
-     */
     protected function loadModule($moduleName)
     {
         $module = Module::find($moduleName);
